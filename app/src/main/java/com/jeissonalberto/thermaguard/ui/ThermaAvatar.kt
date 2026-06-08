355c3327993b546647784431f3c8a21d115df0d4
JeissonAlberto <126131566+JeissonAlberto@users.noreply.github.com>
2026-06-07 22:51:23 -0500

    fix: agregar import Fill de drawscope en ThermaAvatar

--- app/src/main/java/com/jeissonalberto/thermaguard/ui/ThermaAvatar.kt
+++ app/src/main/java/com/jeissonalberto/thermaguard/ui/ThermaAvatar.kt
@@ -19,6 +19,7 @@ import androidx.compose.ui.geometry.RoundRect
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.*
 import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.graphics.drawscope.Fill
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.drawscope.rotate
 import androidx.compose.ui.graphics.drawscope.translate
