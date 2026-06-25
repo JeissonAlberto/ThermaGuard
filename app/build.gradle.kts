ab288e54039d5b858d438c63702b812b91cced3e
JeissonAlberto <126131566+JeissonAlberto@users.noreply.github.com>
2026-06-25 17:23:31 -0500

    chore: release v4.2.2 FINAL

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 195
-        versionName = "4.2.1-EVOLUTION"
+        versionName = "4.2.2-EVOLUTION"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
     }
