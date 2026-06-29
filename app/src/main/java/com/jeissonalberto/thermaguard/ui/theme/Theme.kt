c4192c7f7ce5c332807a6e2e78f749c641f15c04
JeissonAlberto <126131566+JeissonAlberto@users.noreply.github.com>
2026-06-29 16:13:50 -0500

    fix: ultra-stable theme

---
+++ app/src/main/java/com/jeissonalberto/thermaguard/ui/theme/Theme.kt
@@ -0,0 +1,8 @@
+package com.jeissonalberto.thermaguard.ui.theme
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ThermaGuardTheme(content: @Composable () -> Unit) {
+    MaterialTheme(content = content)
+}
