33b5233ef2f92757f8a917dee81ad5e84b32789b
JeissonAlberto <126131566+JeissonAlberto@users.noreply.github.com>
2026-06-29 16:13:51 -0500

    fix: use simple theme in MainActivity

--- app/src/main/java/com/jeissonalberto/thermaguard/MainActivity.kt
+++ app/src/main/java/com/jeissonalberto/thermaguard/MainActivity.kt
@@ -3,21 +3,18 @@ package com.jeissonalberto.thermaguard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import com.jeissonalberto.thermaguard.ui.DashboardScreen
 import com.jeissonalberto.thermaguard.domain.ThermalViewModel
+import com.jeissonalberto.thermaguard.ui.theme.ThermaGuardTheme
 import androidx.lifecycle.viewmodel.compose.viewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MaterialTheme {
-                Surface {
-                    val viewModel: ThermalViewModel = viewModel()
-                    DashboardScreen(viewModel)
-                }
+            ThermaGuardTheme {
+                val viewModel: ThermalViewModel = viewModel()
+                DashboardScreen(viewModel)
             }
         }
     }
