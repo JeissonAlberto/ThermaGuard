6071506dd5ff0d9179f88e56132e38e5d00d36bd
JeissonAlberto <126131566+JeissonAlberto@users.noreply.github.com>
2026-08-11 01:01:36 -0500

    test: include unit tests in Android module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,7 @@ dependencies {
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.kotlinx.coroutines.android)
+    testImplementation(libs.junit)
     
     implementation(libs.androidx.room.runtime)
     implementation(libs.androidx.room.ktx)
